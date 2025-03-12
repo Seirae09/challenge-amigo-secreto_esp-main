5b999ac41b33ef7b4ece10bd464d5d7ad1671932
Isis Gabriela <isgamahi09@gmail.com>
2025-03-11 19:06:52 -0600

    Codigo completo

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//Array para almacenar nombres 
-let amigos [];
+//Array para almacenar nombres y variables
+let amigos = [];
+let campo = document.getElementById("amigo");
+let lista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
+//Funcion para agregar nombres 
+ // Si el campo esta vacio, mostrará alert
+function agregarAmigo(){
+    if (campo.value === "") {
+        alert ("Por favor, escriba un nombre");  
+    } else {
+    amigos.push(campo.value);
+    lista.innerHTML += `<li>${campo.value}</li>`;
+    }
+};
+
+//Sorteo del amigo secreto
+function sortearAmigo(){
+    let sorteo = Math.floor((Math.random() * amigos.length));
+    let amigoSecreto = amigos[sorteo];
+    resultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto} </li>`;
+};
